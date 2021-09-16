44e09e62c3d2e83cd8a2c15797964a835c354f6b
M.O Akindayo <matthewakinse@gmail.com>
2021-09-16 12:46:20 +0300

    Implement button functions

---
+++ quotes-rand/script.js
@@ -0,0 +1,48 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('next-quote');
+
+
+// Fetching quotes from API
+let apiQuotes = [];
+
+async function getQuotes(){
+ 
+  const apiUrl = 'https://type.fit/api/quotes';
+
+  try {
+    const response = await fetch(apiUrl)
+    apiQuotes = await response.json();
+    randQuote();
+  } catch (error) {
+    
+  }
+};
+
+
+
+function randQuote(){
+
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  quote.author ? authorText.textContent = quote.author : authorText.textContent = 'unknown';
+
+  (quote.text.length > 50) ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote')
+  quoteText.textContent = quote.text;
+
+}
+
+// Tweet quote
+function tweetQuote(){
+  const twitterUrl = `http://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', randQuote);
+twitterBtn.addEventListener('click', tweetQuote)
+
+
+// onload call
+getQuotes();
